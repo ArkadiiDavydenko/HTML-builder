6ffd7e616d1bbd53ca0bc98f37f505f8f71eec81
ArkadiiDavydenko <80512176+ArkadiiDavydenko@users.noreply.github.com>
2022-05-22 22:58:58 +0300

    feat: add resolve for task-4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const fsProm = require('fs/promises');
+const mainFolderPath = path.join(__dirname, 'files');
+const copyFolderPath = path.join(__dirname, 'files-copy');
 
+
+
+async function copyDir (mainDir, copyDir) {
+    await fsProm.mkdir(copyDir, {recursive: true});
+
+    const oldFiles = await fsProm.readdir(copyFolderPath);
+    for (let file of oldFiles) {
+        const pathFiles  =path.join(copyFolderPath, file);
+        await fsProm.unlink(pathFiles);
+    }
+
+    const files = await fsProm.readdir(mainFolderPath);
+
+    for (let file of files) {
+        // console.log(file)
+        await fsProm.copyFile(`${mainFolderPath}/${file}`, `${copyFolderPath}/${file}`)
+    }
+
+}
+
+copyDir (mainFolderPath, copyFolderPath);
