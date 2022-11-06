0863ff8aeed5671c08b9c1d7922ccceab1cca18b
ArkadiiDavydenko <80512176+ArkadiiDavydenko@users.noreply.github.com>
2022-11-06 20:19:08 +0300

    add: resolve task 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,3 +2,36 @@ const fs = require('fs');
 const path = require('path');
 const way = path.join(__dirname, 'secret-folder');
 
+function printFileData(name, type, size) {
+    process.stdout.write(`${name} - ${type.slice(1)} - ${size / 1024} kb\n`)
+};
+
+function readDir(filePath) {
+    fs.readdir(filePath, (err, files) => {
+        if (err) console.log(err);
+
+        files.forEach((file) => {
+            // console.log(file)
+            const currentPath = path.join(filePath, file);
+            // console.log(currentPath)
+
+            fs.lstat(currentPath, (err, stat) => {
+                // console.log(stats)
+                if (err) return console.log(err);
+
+                if (stat.isFile()) {
+                    const type = path.extname(file);
+                    const name = path.basename(file, type);
+                    const size = stat.size
+                    printFileData(name, type, size);
+                }
+            })
+
+
+        });
+
+    });
+}
+
+readDir(way);
+
