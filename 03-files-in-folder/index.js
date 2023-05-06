9f3a2efb23acc9637a3473450fb5f14cb2e5d3c6
ArkadiiDavydenko <80512176+ArkadiiDavydenko@users.noreply.github.com>
2023-05-06 12:37:51 +0300

    refactor: refactor margins and spaces in 03 js-file

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,34 +3,28 @@ const path = require('path');
 const way = path.join(__dirname, 'secret-folder');
 
 function printFileData(name, type, size) {
-    process.stdout.write(`${name} - ${type.slice(1)} - ${size / 1024} kb\n`)
-};
+  process.stdout.write(`${name} - ${type.slice(1)} - ${size / 1024} kb\n`);
+}
 
 function readDir(filePath) {
-    fs.readdir(filePath, (err, files) => {
-        if (err) console.log(err);
+  fs.readdir(filePath, (err, files) => {
+    if (err) console.log(err);
 
-        files.forEach((file) => {
+    files.forEach((file) => {
+      const currentPath = path.join(filePath, file);
+      fs.lstat(currentPath, (err, stat) => {
 
-            const currentPath = path.join(filePath, file);
+        if (err) return console.log(err);
 
-
-            fs.lstat(currentPath, (err, stat) => {
-
-                if (err) return console.log(err);
-
-                if (stat.isFile()) {
-                    const type = path.extname(file);
-                    const name = path.basename(file, type);
-                    const size = stat.size
-                    printFileData(name, type, size);
-                }
-            })
-
-
-        });
-
+        if (stat.isFile()) {
+          const type = path.extname(file);
+          const name = path.basename(file, type);
+          const size = stat.size;
+          printFileData(name, type, size);
+        }
+      });
     });
+  });
 }
 
 readDir(way);
