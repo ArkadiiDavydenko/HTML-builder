cd56fb272203d6b1f8022be0f2a067bd9c9d3719
ArkadiiDavydenko <80512176+ArkadiiDavydenko@users.noreply.github.com>
2023-05-06 12:50:22 +0300

    refacor: change structure and remove unnecessary margins in 04; 05; 06 js-files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,24 +1,20 @@
 const path = require('path');
+const fsProm = require('fs/promises');
+
 const mainFolderPath = path.join(__dirname, 'files');
 const copyFolderPath = path.join(__dirname, 'files-copy');
 
-const fsProm = require('fs/promises');
-
 async function copyDir(mainDir, copyDir) {
   await fsProm.mkdir(copyDir, {recursive: true});
-
   const oldFiles = await fsProm.readdir(copyFolderPath);
   for (let file of oldFiles) {
     const pathFiles = path.join(copyFolderPath, file);
     await fsProm.unlink(pathFiles);
   }
-
   const files = await fsProm.readdir(mainFolderPath);
-
   for (let file of files) {
     await fsProm.copyFile(`${mainFolderPath}/${file}`, `${copyFolderPath}/${file}`);
   }
-
 }
 
 copyDir(mainFolderPath, copyFolderPath);
